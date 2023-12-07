ec609f695a058c8572d684531fec7ae28c55dca5
KOX-POLKOKS <biuro.polkoks@gmail.com>
2023-12-07 15:38:51 +0100

    dziala prawie

--- send_email.php
+++ send_email.php
@@ -1,13 +1,11 @@
 <?php
-
-    if(isset($_POST["submit"])){
         $name = $_POST["name"];
         $email = $_POST["email"];
         $tel = $_POST["tel"];
-        $flaws = $_POST["flaws"];
-        $reciever = "zabkarabka506@gmail.com";
-        $subject = "You just got a mail from your site.";
-        $from = "From: " + $email + "\n Tel: " + $tel + "\n" + $name;
+        $message = $_POST["flaws"];
+        $to = "zabkarabka506@gmail.com";
+        $from = "From: $email \n Tel: $tel \n + $name";
+        $subject = "You just got a mail from your site $from";
 
         if(empty($name)){
             header("Location: index.html?error=emptyinputName#malfunction");
@@ -18,20 +16,22 @@
         }elseif(empty($tel)){
             header("Location: index.html?error=emptyinputTel#malfunction");
             exit();
-        }elseif(empty($flaws)){
+        }elseif(empty($message)){
             header("Location: index.html?error=emptyinputFlaws#malfunction");
             exit();
         }else{
-            mail($reciever, $subject, $flaws, $from);
+            mail($to, $subject, $message);
             header("Location: index.html?error=none#malfunction");
             exit();
             
         }
-    }
+  
+
+    
 
 
 
-
+?>
 
 
 
